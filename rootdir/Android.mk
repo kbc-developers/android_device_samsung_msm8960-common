2c14df1cd94fa45484391a5e04ea3c2be1329763
sakuramilk <c.sakuramilk@gmail.com>
2015-05-07 01:47:08 +0900

    Revert "d2dcm: d2dcm use the customized init.qcom.rc"
    This reverts commit e011fe6dbbd8aa18c9b1dbe0bea00fb52aa2d4a4.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
 
-ifneq ($(TARGET_DEVICE),d2dcm)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
@@ -8,7 +7,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
